c236a6a7cd525765dacf02872211c86cda1186eb
Emily <emcruznarva28@gmail.com>
2025-11-21 16:27:26 -0500

    Commit con identidad corregida

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,40 @@
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
+// Importa los componentes de Ionic que realmente usas en el HTML
+import { 
+  IonHeader, 
+  IonToolbar, 
+  IonTitle, 
+  IonContent, 
+  IonText, 
+  IonIcon, 
+  IonButton 
+} from '@ionic/angular/standalone';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+  standalone: true,
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonText,
+    IonIcon,
+    IonButton
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+})
+export class HomePage {
+  message: string | null = null;
+
+  showMessage() {
+    this.message = '¡FELICIDADES! Haz creado tu primera app con Ionic.';
+  }
+}
